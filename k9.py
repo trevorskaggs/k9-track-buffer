057ec20f7d6f7ba59ce2a765f9b932dadfc49627
Trevor Skaggs <skaggs.trevor@gmail.com>
2018-03-15 13:46:03 -0400

    Update to make shapefile

--- k9.py
+++ k9.py
@@ -1,9 +1,8 @@
 import datetime
 import gpxpy
-import geopy
-import random
 import math
-
+import random
+import simplekml
 
 def get_wind_dict(start_time, end_time, lon, lat):
     #TODO Get complete constrained wind dict in single request
@@ -11,7 +10,7 @@ def get_wind_dict(start_time, end_time, lon, lat):
     diff_seconds = (end_time - start_time).total_seconds() + 1
     for minute in range(0, int(diff_seconds)):
         wind_speed = random.randint(5, 10)
-        wind_dir = random.randint(1, 60)
+        wind_dir = random.randint(-30, 30)
         wind_dict[minute] = {'speed': wind_speed, 'dir': wind_dir}
     return wind_dict
 
@@ -19,7 +18,8 @@ def get_pair_point(point, wind_dict):
     wind = wind_dict[(point.time - start_time).total_seconds()]
     R = 6378.1 #Radius of the Earth
     bearing = math.radians(wind['dir'])
-    d = wind['speed'] * 0.003048  # Put the dog magic here
+    d = wind['speed'] * 0.003048 # Convert windspeed at 1 MPH = 3 Meters
+    # Put the dog magic here
 
     lat1 = math.radians(point.latitude) #Current lat point converted to radians
     lon1 = math.radians(point.longitude) #Current long point converted to radians
@@ -32,10 +32,10 @@ def get_pair_point(point, wind_dict):
 
     lat2 = math.degrees(lat2)
     lon2 = math.degrees(lon2)
-    return gpxpy.gpx.GPXTrackPoint(lat2, lon2)
+    return gpxpy.gpx.GPXTrackPoint(lat2, lon2, point.elevation, point.time)
 
-in_file = 'sample-data/track.gpx'
-out_file = 'sample-data/track-out.gpx'
+in_file = 'sample-data/example.gpx'
+out_file = in_file + '.kml'
 gpx_infile = open(in_file)
 gpx = gpxpy.parse(gpx_infile)
 for track in gpx.tracks:
@@ -49,5 +49,8 @@ for track in gpx.tracks:
         for point in current_points:
             if point:
                 seg.points.append(get_pair_point(point, wind_dict))
-gpx_outfile = open(out_file, 'w')
-gpx_outfile.write(gpx.to_xml())
+all_points = [(point.longitude, point.latitude) for point in seg.points]
+all_points.append((seg.points[0].longitude, seg.points[0].latitude))
+kml = simplekml.Kml()
+kml.newpolygon(name='BufferedK9Trail', outerboundaryis=all_points)
+kml.save(out_file)
