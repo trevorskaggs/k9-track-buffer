8c2fc353d6d3ee0ad610988f8cdedd3d8f3c6dba
Trevor Skaggs <skaggs.trevor@gmail.com>
2018-03-17 10:12:01 -0400

    Read real data and add spacing logic.

--- k9.py
+++ k9.py
@@ -1,3 +1,4 @@
+import csv
 import datetime
 import gpxpy
 import math
@@ -5,20 +6,24 @@ import random
 import simplekml
 
 def get_wind_dict(start_time, end_time, lon, lat):
-    #TODO Get complete constrained wind dict in single request
-    wind_dict = {}
-    diff_seconds = (end_time - start_time).total_seconds() + 1
-    for minute in range(0, int(diff_seconds)):
-        wind_speed = random.randint(5, 10)
-        wind_dir = random.randint(-30, 30)
-        wind_dict[minute] = {'speed': wind_speed, 'dir': wind_dir}
+    #TODO Get actual data...
+    with open('sample-data/kestrel-data.csv', 'rb') as windfile:
+        windreader = csv.reader(windfile, delimiter=',', quotechar='|')
+        header_length = 11
+        wind_dict = {}
+        #Time,Temp,Wet Bulb Temp.,Rel. Hum.,Baro.,Altitude,Station P.,Wind Speed,Heat Index,Dew Point,Dens. Alt.,Crosswind,Headwind,Mag. Dir.,True Dir.,Wind Chill,
+        for row in windreader:
+            if windreader.line_num > header_length and row:
+                time = datetime.datetime.strptime(row[0],'%Y-%m-%d %H:%M:%S')
+                #wind_dict[time] = {'speed':row[7], 'dir': row[13]} Valid when complete file
+                wind_dict[time] = {'speed':random.randint(0, 5), 'dir': random.randint(-10, 10)} #Random generation to not change sourcefile
     return wind_dict
 
 def get_pair_point(point, wind_dict):
-    wind = wind_dict[(point.time - start_time).total_seconds()]
+    wind = wind_dict[point.time] if point.time in wind_dict else wind_dict[min(wind_dict.keys(), key=lambda k: abs(k-point.time))]
     R = 6378.1 #Radius of the Earth
     bearing = math.radians(wind['dir'])
-    d = wind['speed'] * 0.003048 # Convert windspeed at 1 MPH = 3 Meters
+    d = wind['speed'] * 0.005000 # Convert windspeed at 1 m/s = 5 Meters
     # Put the dog magic here
 
     lat1 = math.radians(point.latitude) #Current lat point converted to radians
@@ -45,10 +50,15 @@ for track in gpx.tracks:
         max_lon = max(point.longitude for point in seg.points)
         min_lat = min(point.latitude for point in seg.points)
         wind_dict = get_wind_dict(start_time, end_time, max_lon, min_lat)
+        last_valid = None
         current_points = reversed(seg.points)
+         #Set initial valid point
         for point in current_points:
             if point:
-                seg.points.append(get_pair_point(point, wind_dict))
+                # Check if point is far enough away to create a new point.  
+                if not last_valid or ((abs(point.latitude - last_valid.latitude) > .00000001 or abs(point.longitude - last_valid.longitude) > .00000001) and abs((last_valid.time - point.time).total_seconds()) > 30):
+                    last_valid = point
+                    seg.points.append(get_pair_point(point, wind_dict))
 all_points = [(point.longitude, point.latitude) for point in seg.points]
 all_points.append((seg.points[0].longitude, seg.points[0].latitude))
 kml = simplekml.Kml()
