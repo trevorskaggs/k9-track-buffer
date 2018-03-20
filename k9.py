ff64db2afd324f596f4da72c4b2018b9695faacb
Trevor Skaggs <skaggs.trevor@gmail.com>
2018-03-20 09:55:27 -0400

    Build out POD dict

--- k9.py
+++ k9.py
@@ -1,8 +1,7 @@
 import datetime
 import gpxpy
-from shapely.geometry import LinearRing, Polygon
-import simplekml
-import utils
+from osgeo import ogr
+from utils import get_pod_range, get_wind, get_wind_dict
 
 file_name = 'example.gpx'
 in_file = 'sample-data/' + file_name
@@ -18,25 +17,17 @@ r"""<?xml version="1.0" encoding="UTF-8"?>
         </Placemark>
     </Document>
 </kml>"""
+
+wind_dict = get_wind_dict()
+all_points = []
+trail = ogr.Geometry(ogr.wkbLineString)
 gpx_infile = open(in_file)
 gpx = gpxpy.parse(gpx_infile)
 for track in gpx.tracks:
     for seg in track.segments:
-        wind_dict = utils.get_wind_dict()
-        last_valid = None
-        current_points = reversed(seg.points)
-         #Set initial valid point
-        for point in current_points:
+        for point in seg.points:
             if point:
-                # Check if point is far enough away (space/time) to create a new point.  
-                if not last_valid or ((abs(point.latitude - last_valid.latitude) > .00001 and abs(point.longitude - last_valid.longitude) > .00001) and abs((last_valid.time - point.time).total_seconds()) > 15):
-                    last_valid = point
-                    seg.points.append(utils.get_pair_point(point, wind_dict))
-all_points = [(point.longitude, point.latitude) for point in seg.points]
-all_points.append((seg.points[0].longitude, seg.points[0].latitude))
-kml = simplekml.Kml()
-pol = kml.newpolygon(name='A Polygon')
-pol.outerboundaryis.coords = all_points
-kml.save('Track.kml')
-
-
+                pod = get_pod_range('D', 'C', 3, get_wind(point, wind_dict))
+                trail.AddPoint(point.longitude, point.latitude)
+with open(out_file, 'wb') as outfile:
+    outfile.write(kml_template % trail.ExportToKML())
--- utils.py
+++ utils.py
@@ -18,9 +18,11 @@ def get_wind_dict():
                 wind_dict[time] = {'speed':random.randint(0, 5), 'dir': random.randint(-10, 10)} #Random generation to not change sourcefile
     return wind_dict
 
+def get_wind(point, wind_dict):
+    return wind_dict[point.time] if point.time in wind_dict else wind_dict[min(wind_dict.keys(), key=lambda k: abs(k-point.time))]
 
 def get_pair_point(point, wind_dict):
-    wind = wind_dict[point.time] if point.time in wind_dict else wind_dict[min(wind_dict.keys(), key=lambda k: abs(k-point.time))]
+    wind = get_wind(point, wind_dict)
     R = 6378.1 #Radius of the Earth
     bearing = math.radians(wind['dir'])
     d = wind['speed'] * 0.005000 # Convert windspeed at 1 m/s = 5 Meters
@@ -34,6 +36,135 @@ def get_pair_point(point, wind_dict):
     lat2 = math.degrees(lat2)
     lon2 = math.degrees(lon2)
     return gpxpy.gpx.GPXTrackPoint(lat2, lon2, point.elevation, point.time)
+
+def get_pod_range(day_status, cloud_coverage, shadow_length, wind):
+    if day_status == 'D':
+        if cloud_coverage == 'C':
+            if shadow_length < 3.5:
+                if wind < 4:
+                    return 5, 25
+                elif wind >= 4 and wind < 7:
+                    return 7, 27
+                elif wind >= 7 and wind < 10:
+                    return 10, 30
+                elif wind >= 10 and wind < 14:
+                    return 35, 45
+                elif wind >= 14:
+                    return 35, 45
+            elif shadow_length >= 3.5 and shadow_length < 8.5:
+                if wind < 4:
+                    return 7, 27
+                elif wind >= 4 and wind < 7:
+                    return 10, 30
+                elif wind >= 7 and wind < 10:
+                    return 20, 40
+                elif wind >= 10 and wind < 14:
+                    return 55, 65
+                elif wind >= 14:
+                    return 80, 85
+            elif shadow_length <= 8.5:
+                if wind < 4:
+                    return 10, 30
+                elif wind >= 4 and wind < 7:
+                    return 35, 45
+                elif wind >= 7 and wind < 10:
+                    return 35, 45
+                elif wind >= 10 and wind < 14:
+                    return 80, 85
+                elif wind >= 14:
+                    return 80, 85
+        elif cloud_coverage == 'M':
+            if shadow_length < 3.5:
+                if wind < 4:
+                    return 7, 27
+                elif wind >= 4 and wind < 7:
+                    return 10, 30
+                elif wind >= 7 and wind < 10:
+                    return 20, 40
+                elif wind >= 10 and wind < 14:
+                    return 55, 65
+                elif wind >= 14:
+                    return 80, 85
+            elif shadow_length >= 3.5 and shadow_length < 8.5:
+                if wind < 4:
+                    return 10, 30
+                elif wind >= 4 and wind < 7:
+                    return 35, 45
+                elif wind >= 7 and wind < 10:
+                    return 35, 45
+                elif wind >= 10 and wind < 14:
+                    return 55, 65
+                elif wind >= 14:
+                    return 80, 85
+            elif shadow_length <= 8.5:
+                if wind < 4:
+                    return 80, 85
+                elif wind >= 4 and wind < 7:
+                    return 80, 85
+                elif wind >= 7 and wind < 10:
+                    return 80, 85
+                elif wind >= 10 and wind < 14:
+                    return 80, 85
+                elif wind >= 14:
+                    return 80, 85
+        elif cloud_coverage == 'M':
+            if shadow_length < 3.5:
+                if wind < 4:
+                    return 10, 30
+                elif wind >= 4 and wind < 7:
+                    return 35, 45
+                elif wind >= 7 and wind < 10:
+                    return 35, 45
+                elif wind >= 10 and wind < 14:
+                    return 80, 85
+                elif wind >= 14:
+                    return 80, 85
+            elif shadow_length >= 3.5 and shadow_length < 8.5:
+                if wind < 4:
+                    return 80, 85
+                elif wind >= 4 and wind < 7:
+                    return 80, 85
+                elif wind >= 7 and wind < 10:
+                    return 80, 85
+                elif wind >= 10 and wind < 14:
+                    return 80, 85
+                elif wind >= 14:
+                    return 80, 85
+            elif shadow_length <= 8.5:
+                if wind < 4:
+                    return 80, 85
+                elif wind >= 4 and wind < 7:
+                    return 80, 85
+                elif wind >= 7 and wind < 10:
+                    return 80, 85
+                elif wind >= 10 and wind < 14:
+                    return 80, 85
+                elif wind >= 14:
+                    return 80, 85
+    elif day_status == 'N':
+        if cloud_coverage == 'C':
+            if wind >= 0 and wind < 7:
+                return 95, 96
+            elif wind >= 7 and wind < 10:
+                return 90, 92
+            elif wind >= 10 and wind < 14:
+                return 80, 85
+            elif wind >= 14:
+                return 80, 85
+        elif cloud_coverage == 'M':
+            if wind >= 0 and wind < 7:
+                return 90, 92
+            elif wind >= 7 and wind < 10:
+                return 80, 85
+            elif wind >= 10 and wind < 14:
+                return 80, 85
+            elif wind >= 14:
+                return 80, 85
+
+
+LOW_PROBABILITY = 25 # and < 50
+MEDIUM_PROBABILITY = 50 # and < 75
+HIGH_PROBABILITY = 80 # and < 100
 
 POD_STABILITY_LOOKUP_DICT = {
             'A':{
